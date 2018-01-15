65b5cf77224545df0532bd9c0ec539d3ab4601b9
Mashinarius <mashinarius@gmail.com>
2018-01-15 23:10:18 +0300

    time resp

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,8 +1,16 @@
 package com.example.demo;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Response;
 
 @RestController
 public class GreetingController {
@@ -14,5 +22,12 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @RequestMapping("/time")
+    public ResponseEntity<String> getTime() {
+        String dateTime = ZonedDateTime.now(ZoneId.of("Europe/Minsk")).toString();
+        return  new ResponseEntity<>(dateTime, HttpStatus.OK);
+
     }
 }
